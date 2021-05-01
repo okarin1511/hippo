81aff47720cd75eba29b67cc316b58fb536d401e
okarin1511 <69676297+okarin1511@users.noreply.github.com>
2021-05-01 17:16:51 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname= "App">
+      <Header />
+      <Home />
+      {/* <Contact/> */}
+      
     </div>
   );
 }
