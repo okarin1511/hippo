8e4c8d0f85410b374ee38187853ab056a981d74a
okarin1511 <69676297+okarin1511@users.noreply.github.com>
2021-05-02 12:34:34 +0530

    Add files via upload

---
+++ src/pages/MenuItems.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'; 
+import Contact from './Contact';
+export const MenuItems = [
+    {
+        title:'About',
+        url: '#A',
+        cName: 'nav-links'
+        
+    },
+
+    {
+        title:'Benefits',
+        url: '#B',
+        cName: 'nav-links'
+        
+    },
+
+    {
+        title:'Contact Us',
+        url: '#C',
+        cName: 'nav-links'
+    },
+
+    // {
+    //     title:'Noodles',
+    //     url: 'https://youtu.be/gElJoZHod-U',
+    //     cName: 'nav-links'
+    // },
+
+    // {
+    //     title:'Poodles',
+    //     url: 'https://youtu.be/D-cXOEs6I6M',
+    //     cName: 'nav-links'
+    // },
+
+
+]
